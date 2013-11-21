7ea990df8262211250069d0ca312a196fba4a3c9
Mason Stewart <masondesu@gmail.com>
2013-11-21 09:27:33 -0500

    Fixed bad asset paths on css images

--- Gruntfile.js
+++ Gruntfile.js
@@ -123,7 +123,7 @@ module.exports = function (grunt) {
             },
             dist: {
                 options: {
-                    httpImagesPath: '../images',
+                    httpImagesPath: '/beerfest/images',
                     generatedImagesDir: '<%= yeoman.dist %>/images/generated'
                 }
             },
